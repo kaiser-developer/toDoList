12a3b38ff17b15f79bbb0ad9099fea0799ac84c7
Treinamento <Treinamento@RD723921.raiadrogasil.com>
2020-02-05 17:25:42 -0300

    Botão 'adicionar tarefa' feito

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let btnAddTarefa = document.getElementById("adicionar-tarefa");
+let btnRmvTarefa = document.getElementById("botao-tarefa");
+let inputTarefa = document.getElementById("tarefa-digitada");
+let novaTarefa;
+let divTarefas = document.getElementById("tarefas");
+
+btnAddTarefa.onclick = () => {
+    novaTarefa = `<div class="col-md-4">
+                    <div class="card-tarefa">
+                        <div class="text-tarefa">
+                            ${inputTarefa.value}
+                        </div>
+                        <div class="botao-tarefa">
+                            <img src="images/check.png" alt="Botão para finalizar tarefa" title="Botão para finalizar tarefa" width="32">
+                        </div>
+                    </div>
+                </div>`
+    divTarefas.innerHTML += novaTarefa;
+    inputTarefa.value = "";
+}
