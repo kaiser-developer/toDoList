8e2b5a6c12fb2a3f7911e6e5b8132f2f99265d2a
Treinamento <Treinamento@RD723921.raiadrogasil.com>
2020-02-06 09:27:53 -0300

    Todas as funcionalidades foram completadas

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
 let btnAddTarefa = document.getElementById("adicionar-tarefa");
-let btnRmvTarefa = document.getElementById("botao-tarefa");
 let inputTarefa = document.getElementById("tarefa-digitada");
-let novaTarefa;
 let divTarefas = document.getElementById("tarefas");
+let qtdTarefas = 0;
 
 btnAddTarefa.onclick = () => {
+    adicionarTarefa();
+}
+
+inputTarefa.onkeypress = (e) =>{
+    if (e.which == 13) {
+        adicionarTarefa();
+    }
+}
+
+removerTarefa = (tarefa) => {
+    let id = tarefa.getAttribute("value");
+    let div = document.getElementById(id);
+    div.remove();
+}
+
+adicionarTarefa = () => {
     if (inputTarefa.value == ""){
         alert("INSIRA ALGUM DADO");
         return;
     }
-    novaTarefa = `<div class="col-md-4">
+    
+    let novaTarefa = `<div class="col-md-4 divTarefas" id="tarefa${qtdTarefas}">
                     <div class="card-tarefa">
                         <div class="text-tarefa">
                             ${inputTarefa.value}
                         </div>
-                        <div class="botao-tarefa">
+                        <div class="botao-tarefa" value="tarefa${qtdTarefas}" onClick="removerTarefa(this);">
                             <img src="images/check.png" alt="Botão para finalizar tarefa" title="Botão para finalizar tarefa" width="32">
                         </div>
                     </div>
                 </div>`
     divTarefas.innerHTML += novaTarefa;
     inputTarefa.value = "";
+    qtdTarefas++;
 }
